830bd2ccd98fc08ef88e7c3133fbb6a4400cc5f7
shivam vora <59314521+shivamvora@users.noreply.github.com>
2020-09-26 17:04:15 +0530

    Updates

---
+++ precache-manifest.f474887dfc608910bf6062cf1a701b6f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "240010c832c765a29a1bf898681fb04c",
+    "url": "/first_react_design/index.html"
+  },
+  {
+    "revision": "64676e688d061061642c",
+    "url": "/first_react_design/static/css/main.a1bcdfdc.chunk.css"
+  },
+  {
+    "revision": "5d04d44be2a5a0b01187",
+    "url": "/first_react_design/static/js/2.763770c0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/first_react_design/static/js/2.763770c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64676e688d061061642c",
+    "url": "/first_react_design/static/js/main.cfc494bb.chunk.js"
+  },
+  {
+    "revision": "6e372aba3803d6ce40df",
+    "url": "/first_react_design/static/js/runtime-main.60a48b81.js"
+  }
+]);
